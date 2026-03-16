e12f210f7075e46a8a35b18cd1c19254d1ee3174
raghavabharadwaj1 <skandaraghavabharadwaj@gmail.com>
2026-03-16 11:52:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from streamlit_option_menu import option_menu
 
 # ---------------- CONFIG ----------------
 st.set_page_config(page_title="SIP Timing Dashboard", layout="wide")
+# This puts it at the top left of the sidebar
+if os.path.exists("https://craftmycv.com/storage/production/images/logos/companies/kUDcH3wsfCpVhYlowyyW8IbzOC6Ep5lZDSe2FRS6.jpg"): # Change "logo.png" to your actual filename
+    st.sidebar.image("https://craftmycv.com/storage/production/images/logos/companies/kUDcH3wsfCpVhYlowyyW8IbzOC6Ep5lZDSe2FRS6.jpg", use_container_width=True)
+    st.sidebar.markdown("---")
+else:
+    st.sidebar.warning("Logo file not found.")
 
 # ---------------- LOAD DATA ----------------
 @st.cache_data
